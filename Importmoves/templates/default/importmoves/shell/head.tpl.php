2f7f3da7c906f056554a09b632e493383c2de000
Peter <srcr@users.noreply.github.com>
2017-04-20 15:44:16 +0200

    Make use of the already local d3.v3.min.js
    Lose the dependency on cloudflare

--- Importmoves/templates/default/importmoves/shell/head.tpl.php
+++ Importmoves/templates/default/importmoves/shell/head.tpl.php
@@ -1,5 +1,5 @@
 <?php ?>.
 
 <!-- Import Moves -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js" charset="utf-8"></script>
+<script src="<?= Idno\Core\site()->config()->getDisplayURL() ?>IdnoPlugins/Importmoves/external/d3.v3.min.js" charset="utf-8"></script>
 <link rel="stylesheet" href="<?= Idno\Core\site()->config()->getDisplayURL() ?>IdnoPlugins/Importmoves/external/style.css" >
